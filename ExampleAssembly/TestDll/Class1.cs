cd76a4f11a36a8c481be4a4b641795a60bd8864f
Michidu <michido@protonmail.com>
2017-08-01 14:30:26 +0300

    Added example

---
+++ ExampleAssembly/TestDll/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace TestDll
+{
+    public class Load
+    {
+        public static void Init()
+        {
+            string str = "Loaded assemblies:\n";
+
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (Assembly assembly in assemblies)
+            {
+                str += assembly.FullName + "\n";
+            }
+
+            File.WriteAllText("log.txt", str);
+        }
+    }
+}
